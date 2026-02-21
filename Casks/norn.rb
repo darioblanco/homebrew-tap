fc40ca0e75545cf927d3da0acc91a2d8c70b5778
Darío Blanco <dario@gokarla.io>
2026-02-21 12:09:30 +0100

    norn 0.0.1

--- Casks/norn.rb
+++ Casks/norn.rb
@@ -1,6 +1,6 @@
 cask "norn" do
   version "0.0.1"
-  sha256 "1798ddfd86cda7d2ba8759d24fb9ec897304f7141d7f3d56c658972027357553"
+  sha256 "2aced5182aeffc18690fbcd10718635082ebf1bc974637cda88ecdaf2d0949a6"
 
   url "https://releases.darioblanco.com/norn/v#{version}/norn-#{version}-darwin-arm64.dmg"
   name "Norn"
